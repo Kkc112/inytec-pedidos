dbd69595434448a008f993974de7fba174cb1ee8
Kkc112 <cechfrancisco427@gmail.com>
2026-05-29 19:36:33 -0300

    Fix correction uuid validation

--- app/api/orders/[id]/correction/route.js
+++ app/api/orders/[id]/correction/route.js
@@ -95,7 +95,7 @@ function cleanItem(item) {
 }
 
 function isSafeUuid(value) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i.test(value ?? "");
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value ?? "");
 }
 
 async function insertOrderEvents(supabase, orderIds, eventType, payload) {
